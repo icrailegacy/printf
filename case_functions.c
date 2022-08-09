8f7a077a8c3fc8f3d9e303301d5d6bd55f0967e5
Isaac Katusabe <isaackatusabe@gmail.com>
2022-08-09 22:00:52 +0300

    Update case_functions.c

--- case_functions.c
+++ case_functions.c
@@ -14,3 +14,28 @@ int print_character(va_list arg)
 
 	return (1);
 }
+
+/**
+ * print_sign - prints sign
+ * @arg: va_list parameter
+ * @base: base 10, 8, 16, 2 etc..
+ * Description: print numbers signed
+ * Return: num of characters
+ */
+int print_sign(va_list arg, int base)
+{
+	int i = 0, cont = 0;
+	char *s;
+
+	i = va_arg(arg, int);
+	if (i < 0)
+	{
+		i = -(i);
+		_putchar('-');
+		cont += 1;
+	}
+	s = convert_to("0123456789ABCDEF", i, base);
+	_puts(s);
+	cont += _strlen(s);
+	return (cont);
+}
