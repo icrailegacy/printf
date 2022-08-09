baed83dfa297e68d00e5e98dac1a4ee866c30813
Isaac Katusabe <isaackatusabe@gmail.com>
2022-08-10 00:20:06 +0300

    Update custom_functions.c

--- custom_functions.c
+++ custom_functions.c
@@ -45,3 +45,30 @@ void _puts(char *str)
 		_putchar(str[i]);
 	}
 }
+
+/**
+ *convert_to - convert numbers
+ *Description: This function converts numbers to other formats
+ *decimal, octal, hexadecimal, binary etc..
+ *@representation: char representation[] = "0123456789ABCDEF";
+ *@num: num to transform
+ *@base: base to transform num
+ *Return: number into char pointer
+ */
+
+char *convert_to(char representation[], unsigned int num, int base)
+{
+	char *ptr;
+	static char buffer[128];
+	int mod = 0;
+
+	ptr = &buffer[127];
+	*ptr = '\0';
+
+	do {
+		mod = num % base;
+		*--ptr = representation[mod];
+		num /= base;
+	} while (num != 0);
+	return (ptr);
+}
