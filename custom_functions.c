42591f5ae5ddfc8a3d3963a175e1b9107aef381a
Ruthy Agatha <ruthyagatha3@gmail.com>
2022-08-09 23:34:27 +0300

    New updates

--- custom_functions.c
+++ custom_functions.c
@@ -13,3 +13,20 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+/**
+ *_strlen - reset number
+ *Description: This function return a length for some string
+ *@s: pointer char
+ *Return: int length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+	{
+		len++;
+	}
+	return (len);
+}
