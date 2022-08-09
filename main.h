607960e23c792ef96885b56d5d365adef171768b
Ruthy Agatha <ruthyagatha3@gmail.com>
2022-08-09 19:56:23 +0300

    printf Team project

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_MAIN
+#define HEADER_MAIN
+#include <stdarg.h>
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
+int _printf(const char *format, ...);
+char *convert_to(char representation[], unsigned int num, int base);
+int function_manager(char c, va_list arg);
+int _switch(char c, va_list arg);
+int print_character(va_list arg);
